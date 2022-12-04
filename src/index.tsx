205981db3d2a0ead398f6fefdd6a81672d968f33
Dawid Domiński <dawid.dominski1125@gmail.com>
2022-12-04 12:12:15 +0100

    feat: remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {store} from './components/redux/store'
-import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
+	<App />
 )
 

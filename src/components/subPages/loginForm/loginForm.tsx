d1f16adbae993a939d930adbf7b4893cb21319fb
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-02 22:17:09 +0100

    feat: change loginForm design, give info about incorrect labels

--- src/components/subPages/loginForm/loginForm.tsx
+++ src/components/subPages/loginForm/loginForm.tsx
@@ -1,39 +1,101 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import {
+	FormControl,
+	IconButton,
+	InputAdornment,
+	TextField,
+} from '@mui/material'
 import useLoginForm from './useLoginForm'
 import styles from './loginForm.module.scss'
 import { Link } from 'react-router-dom'
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 
 const LoginForm = () => {
-	const { handleChange, loginForm, login } = useLoginForm()
+	const {
+		handleChange,
+		loginForm,
+		login,
+		showPassword,
+		handleClickShowPassword,
+		handleMouseDownPassword,
+		userExist,
+	} = useLoginForm()
 
 	return (
 		<div className={styles.loginForm}>
-			<form noValidate autoComplete='off'>
-				<TextField
-					label='login'
-					variant='outlined'
-					value={loginForm.login}
-					name='login'
-					onChange={handleChange}
-					required
-				/>
-				<TextField
-					label='Password'
-					variant='outlined'
-					value={loginForm.password}
-					name='password'
-					type='password'
-					onChange={handleChange}
-					required
-				/>
-			</form>
-			<div>
-				<button onClick={() => login()}>Login</button>
+			<div className={styles.loginBackground}>
+				<form noValidate autoComplete='off'>
+					<FormControl sx={{ margin: 'normal' }}>
+						<TextField
+							label='login'
+							variant='outlined'
+							value={loginForm.login}
+							name='login'
+							className={styles.loginLabel}
+							error={!userExist.login}
+							helperText={
+								!userExist.login &&
+								loginForm.login.length > 0 &&
+								'User does not exist!'
+							}
+							onChange={handleChange}
+							required
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position='start'>
+										<AccountCircle />
+									</InputAdornment>
+								),
+							}}
+						/>
+					</FormControl>
+					<FormControl sx={{ margin: 'normal' }}>
+						<TextField
+							label='password'
+							variant='outlined'
+							type={showPassword ? 'text' : 'password'}
+							value={loginForm.password}
+							className={styles.loginLabel}
+							name='password'
+							onChange={handleChange}
+							error={!userExist.password}
+							helperText={!userExist.password && 'Incorrect password!'}
+							required
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position='start'>
+										<LockOutlinedIcon />
+									</InputAdornment>
+								),
+								endAdornment: (
+									<InputAdornment position='end'>
+										<IconButton
+											aria-label='toggle password visibility'
+											onClick={handleClickShowPassword}
+											onMouseDown={handleMouseDownPassword}
+											edge='end'
+										>
+											{showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
+									</InputAdornment>
+								),
+							}}
+						/>
+					</FormControl>
+				</form>
+				<div>
+					<button className={styles.register} onClick={() => login()}>
+						Login
+					</button>
+				</div>
+				<Link to={'/register'} className={styles.createAccountLink}>
+					<div className={styles.createAccount}>
+						Dont have account? Register now
+					</div>
+				</Link>
 			</div>
-			<Link to={'/register'} className={styles.createAccount}>
-				<div>Dont have account? Register now</div>
-			</Link>
 		</div>
 	)
 }
--- src/components/subPages/loginForm/useLoginForm.ts
+++ src/components/subPages/loginForm/useLoginForm.ts
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
 import { store } from '../../../redux/store'
 import { getClientResponse } from '../../../redux/user'
+import axios from 'axios'
 
 const useLoginForm = () => {
 	const navigate = useNavigate()
@@ -14,11 +15,30 @@ const useLoginForm = () => {
 		password: '',
 	})
 
+	const [userExist, setUserExist] = useState({
+		login: true,
+		password: true,
+	})
+
+	const [showPassword, setShowPassword] = React.useState(false)
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+	const handleMouseDownPassword = (
+		event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		event.preventDefault()
+	}
+
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = event.target
 		setLoginForm((prevState) => ({
 			...prevState,
 			[name]: value,
+		}))
+		setUserExist((prevState) => ({
+			...prevState,
+			[name]: true,
 		}))
 	}
 
@@ -36,17 +56,35 @@ const useLoginForm = () => {
 				store.dispatch(getClientResponse({ isLogged: true }))
 				navigate('/')
 			},
-			onError: () => {
+			onError: (error) => {
 				store.dispatch(getClientResponse({ isLogged: false }))
-				toast.error('Login failed')
+				if (axios.isAxiosError(error)) {
+					if (error.response?.data.errorCode === 'user-does-not-exist') {
+						setUserExist({
+							...userExist,
+							login: false,
+						})
+					} else if (error.response?.data.errorCode === 'incorrect-password') {
+						setUserExist({
+							...userExist,
+							password: false,
+						})
+					}
+				} else {
+					toast.error('Login failed')
+				}
 			},
 		}
 	)
 
 	return {
+		showPassword,
+		handleClickShowPassword,
+		handleMouseDownPassword,
 		handleChange,
 		loginForm,
 		login,
+		userExist,
 	}
 }
 

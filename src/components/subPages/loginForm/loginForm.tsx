9ca3b84230848b4e0ea3f37d9b90a36687fbc80b
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-03-07 02:49:32 +0100

    feat: add custom mui theme to login form

--- src/components/subPages/loginForm/loginForm.tsx
+++ src/components/subPages/loginForm/loginForm.tsx
@@ -11,6 +11,8 @@ import { Link } from 'react-router-dom'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import { Visibility, VisibilityOff } from '@mui/icons-material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import { ThemeProvider } from '@mui/material/styles'
+import { purpleTheme } from '../../themes/customMuiThemes'
 
 const LoginForm = () => {
 	const {
@@ -27,63 +29,68 @@ const LoginForm = () => {
 		<div className={styles.loginForm}>
 			<div className={styles.loginBackground}>
 				<form noValidate autoComplete='off'>
-					<FormControl sx={{ margin: 'normal' }}>
-						<TextField
-							label='login'
-							variant='outlined'
-							value={loginForm.login}
-							name='login'
-							className={styles.loginLabel}
-							error={!userExist.login}
-							helperText={
-								!userExist.login &&
-								loginForm.login.length > 0 &&
-								'User does not exist!'
-							}
-							onChange={handleChange}
-							required
-							InputProps={{
-								startAdornment: (
-									<InputAdornment position='start'>
-										<AccountCircle />
-									</InputAdornment>
-								),
-							}}
-						/>
-					</FormControl>
-					<FormControl sx={{ margin: 'normal' }}>
-						<TextField
-							label='password'
-							variant='outlined'
-							type={showPassword ? 'text' : 'password'}
-							value={loginForm.password}
-							className={styles.loginLabel}
-							name='password'
-							onChange={handleChange}
-							error={!userExist.password}
-							helperText={!userExist.password && 'Incorrect password!'}
-							required
-							InputProps={{
-								startAdornment: (
-									<InputAdornment position='start'>
-										<LockOutlinedIcon />
-									</InputAdornment>
-								),
-								endAdornment: (
-									<InputAdornment position='end'>
-										<IconButton
-											aria-label='toggle password visibility'
-											onClick={handleClickShowPassword}
-											onMouseDown={handleMouseDownPassword}
-											edge='end'
-										>
-											{showPassword ? <VisibilityOff /> : <Visibility />}
-										</IconButton>
-									</InputAdornment>
-								),
-							}}
-						/>
-					</FormControl>
+					<ThemeProvider theme={purpleTheme}>
+						<FormControl sx={{ margin: 'normal' }}>
+							<TextField
+								label='login'
+								variant='outlined'
+								value={loginForm.login}
+								name='login'
+								className={styles.loginLabel}
+								error={!userExist.login}
+								helperText={
+									!userExist.login &&
+									loginForm.login.length > 0 &&
+									'User does not exist!'
+								}
+								onChange={handleChange}
+								required
+								focused
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position='start'>
+											<AccountCircle style={{ color: '#FFFFFF' }} />
+										</InputAdornment>
+									),
+								}}
+							/>
+						</FormControl>
+						<FormControl sx={{ margin: 'normal' }}>
+							<TextField
+								label='password'
+								variant='outlined'
+								type={showPassword ? 'text' : 'password'}
+								value={loginForm.password}
+								className={styles.loginLabel}
+								name='password'
+								onChange={handleChange}
+								error={!userExist.password}
+								helperText={!userExist.password && 'Incorrect password!'}
+								required
+								focused
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position='start'>
+											<LockOutlinedIcon style={{ color: '#FFFFFF' }} />
+										</InputAdornment>
+									),
+									endAdornment: (
+										<InputAdornment position='end'>
+											<IconButton
+												aria-label='toggle password visibility'
+												onClick={handleClickShowPassword}
+												onMouseDown={handleMouseDownPassword}
+												edge='end'
+												style={{ color: '#FFFFFF' }}
+											>
+												{showPassword ? <VisibilityOff /> : <Visibility />}
+											</IconButton>
+										</InputAdornment>
+									),
+								}}
+							/>
+						</FormControl>
+					</ThemeProvider>
 				</form>
 				<div>
 					<button className={styles.login} onClick={() => login()}>
